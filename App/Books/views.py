7e6f075164be64b6ba0922d80ac13a69e7528348
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-16 17:48:30 +0100

    ALL BOOKS endpoint

--- App/Books/views.py
+++ App/Books/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from serializers import BookSerializer, AuthorSerializer
+from .models import Book, Author
 
 # Create your views here.
+
+
+@api_view(("GET",))
+def all_books(request):
+    books = Book.objects.all()
+    serializer = BookSerializer(Book, many=True)
+    return JsonResponse(serializer.data, safe=False)
