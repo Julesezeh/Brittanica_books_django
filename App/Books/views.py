1396c53d5e51e050de2c298836dd0711a31fd935
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-18 20:34:28 +0100

    Author views

--- App/Books/views.py
+++ App/Books/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .serializers import BookSerializer, AuthorSerializer
 from .models import Book, Author
+from rest_framework.views import APIView
 
 # Create your views here.
 
@@ -12,3 +14,22 @@ def all_books(request):
     books = Book.objects.all()
     serializer = BookSerializer(books, many=True)
     return JsonResponse(serializer.data, safe=False)
+
+
+class AuthorViews(APIView):
+    def get(self, reuquest):
+        all = Author.objects.all()
+        serializer = AuthorSerializer(all)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        try:
+            serializer = AuthorSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            else:
+                with Exception as e:
+                    return Response(e)
+        except Exception as e:
+            return Response(e)
