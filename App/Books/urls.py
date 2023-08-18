f79e9aa2d92605fb938f99d906e083c414eca09e
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-18 22:22:08 +0100

    Deleted app

--- App/Books/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import all_books, AuthorViews
-
-urlpatterns = [
-    path("v1/books/", all_books, name="all_books"),
-    # path("v1/users/<str:user_id>/books/", add_book, name="new_book"),
-    path("v1/users/", AuthorViews.as_view(), name="Authors"),
-]
