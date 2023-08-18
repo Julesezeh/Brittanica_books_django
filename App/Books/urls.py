e18d151be1972d67185f5eb1e949a76079291940
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-18 20:34:12 +0100

    Author view routes

--- App/Books/urls.py
+++ App/Books/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
-from .views import all_books
+from .views import all_books, AuthorViews
 
-urlpatterns = [path("books", all_books, name="all_books")]
+urlpatterns = [
+    path("v1/books/", all_books, name="all_books"),
+    # path("v1/users/<str:user_id>/books/", add_book, name="new_book"),
+    path("v1/users/", AuthorViews.as_view(), name="Authors"),
+]
