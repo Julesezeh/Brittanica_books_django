2e313eb62f3d8a296b48c9d14630e98faecd2d92
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-18 20:33:27 +0100

    added validator and custom saver

--- App/Books/serializers.py
+++ App/Books/serializers.py
@@ -8,7 +8,21 @@ class BookSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class AuthorSerializer(serializers.ModelSerializer):
+class AuthorSerializer(serializers.Serializer):
     class Meta:
         model = Author
         fields = "__all__"
+
+    def validate(self, data):
+        if not data.get("username"):
+            return serializers.ValidationError("username field is required")
+        if not data.get("first_name"):
+            raise serializers.ValidationError("first_name field is required")
+        if not data.get("last_name"):
+            raise serializers.ValidationError("last_name field is required")
+        if not data.get("email"):
+            raise serializers.ValidationError("email field is required")
+        return data
+
+    def create(self, validated_data):
+        return Author.objects.create(**validated_data)
