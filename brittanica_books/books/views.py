bf1b0e82f3d6b812d8d76956cb31f52693068d08
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-19 09:16:05 +0100

    Books view done

--- brittanica_books/books/views.py
+++ brittanica_books/books/views.py
@@ -23,4 +23,20 @@ class UserView(APIView):
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    
+class BooksView(APIView):
+    def get(self,request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    def post(self,request):
+        try:
+            serializer = BookSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,status=status.HTTP_201_CREATED)
+            
+            else:
+                return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            
+        except Exception as e:
+            return Response({"error":str(e)},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
