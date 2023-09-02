92b7a05a9a08c848593a25156661ba6144116388
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-09-02 14:04:43 +0100

    docs requirements

--- brittanica_books/brittanica_books/urls.py
+++ brittanica_books/brittanica_books/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
+
+
+schema_view = get_schema_view(title='Books API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/',include('books.urls'))
+    path('api/v1/',include('books.urls')),
+    path('docs/',include_docs_urls(title='Blog API')),
+    path('schema/', schema_view)
 ]
